83d799f2514c47726ed9782c782e89189b1ba773
cola2261 <cola2261@proton.me>
2025-10-29 00:41:48 +0500

    sm8550: Move some OFOX settings from dm2q DT https://github.com/cola2261/android_device_samsung_dm2q-recovery/tree/fox_12.1
    Signed-off-by: cola2261 <cola2261@proton.me>

--- fox_sm8550.mk
+++ fox_sm8550.mk
@@ -1,2 +1,15 @@
-# Flashlight path
+# Maintainer
+OF_MAINTAINER := cola2261
+
+# FBE decryption
+OF_SKIP_FBE_DECRYPTION := 1
+
+# LPTools
+OF_ENABLE_LPTOOLS := 1
+
+# lz4 compression
+OF_USE_LZ4_COMPRESSION := 1
+
+# Flashlight
+OF_FLASHLIGHT_ENABLE := 1
 OF_FL_PATH1 := /tmp/flashlight # See /init.recovery.qcom.rc for more information
